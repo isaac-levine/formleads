aa95f587f3b7752d3c119ebddf40a052a858bf89
Isaac Levine <isaacmlevine4@gmail.com>
2024-12-30 18:16:56 -0500

    messing around

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
 import "@/components/craft/craft.css";
 import { Main } from "@/components/craft";
-import Hero from "@/components/hero";
+// import Hero from "@/components/hero";
+import Hero2 from "@/components/hero2";
 import CTA from "@/components/landing/cta";
 import Footer from "@/components/footer";
-import Feature from "@/components/landing/feature";
+// import Feature from "@/components/landing/feature";
 import FAQ from "@/components/landing/faq";
-import Header from "@/components/landing/header";
+// import Header from "@/components/landing/header";
+import Hero6 from "@/components/landing/hero6";
+import Features from "@/components/landing/features";
+import { TypewriterEffectSmoothDemo } from "@/components/landing/TypewriterEffectSmoothDemo";
+// import Pricing from "@/components/landing/pricing";
 // import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 
 export default function Home() {
   return (
     <>
       <Main className="pt-12">
-        <Hero />
+        <TypewriterEffectSmoothDemo />
+        {/* <Hero /> */}
+        <Hero2 />
         {/* <AnimatedTooltip items={[]} /> */}
-        <Header />
-        <Feature />
+        {/* <Header /> */}
+        {/* <Feature /> */}
+        <Features />
+        <Hero6 />
         <FAQ />
+        {/* <Pricing /> */}
         <CTA />
       </Main>
       <Footer />
